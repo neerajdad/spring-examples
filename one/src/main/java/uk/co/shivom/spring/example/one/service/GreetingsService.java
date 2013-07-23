299f2c9c31d39b19a77f5afabf371d08c7a12480
neerajdad <neerajdad@gmail.com>
2013-07-23 17:29:28 +0100

    added the PostConstruct and PreDestroy (The annotations are not shipped with Spring Context, updated maven dependency to include JSR 250

--- one/src/main/java/uk/co/shivom/spring/example/one/service/GreetingsService.java
+++ one/src/main/java/uk/co/shivom/spring/example/one/service/GreetingsService.java
@@ -1,5 +1,8 @@
 package uk.co.shivom.spring.example.one.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Service;
 
 import uk.co.shivom.spring.example.one.model.Person;
@@ -11,4 +14,13 @@ public class GreetingsService {
 		System.out.println("Howdy "+ p.getName()+" !!!!");
 	}
 	
+	@PostConstruct
+	public void init() {
+		System.out.println("Initialising the service bean ......");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("destroying the service bean ......");
+	}
 }
